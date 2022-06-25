1aa893949a9137bfc255ff264bcf316b17866582
lishao <285051863@qq.com>
2022-06-25 17:52:41 +0800

    模型迁移+tables创建完成

--- dailyfresh/dailyfresh/settings.py
+++ dailyfresh/dailyfresh/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 富文本编辑器
+    'tinymce',
+    # 注册子应用
+    'apps.users',  # 用户模块
+    'apps.goods',  # 商品模块
+    'apps.cart',   # 购物车模块
+    'apps.order',  # 订单模块
 ]
 
 MIDDLEWARE = [
@@ -54,7 +62,7 @@ ROOT_URLCONF = 'dailyfresh.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],   # 配置模板
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,11 +83,18 @@ WSGI_APPLICATION = 'dailyfresh.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dailyfresh',
+        'USER': 'lishao',
+        'PASSWORD': '123456',
+        'HOST':'127.0.0.1',
+        'PORT':3306,
     }
 }
 
+# Django认证系统使用的模型类，因为User类中使用了系统中的AbstractUser，否则迁移会报错
+# 不再使用django自动生成的auth_user表，而是使用自己定义的类，createsuperuser的时候会加到自己的类中
+AUTH_USER_MODEL = 'users.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -103,9 +118,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
@@ -118,8 +133,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATICFILES_DIRS = BASE_DIR/'static'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# ------------富文本编辑器配置-------------
+TINYMCE_DEFAULT_CONFIG = {
+    'theme': 'advanced',
+    'width': 600,
+    'height': 400,
+}
+
+
--- dailyfresh/dailyfresh/urls.py
+++ dailyfresh/dailyfresh/urls.py
@@ -13,9 +13,31 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# from django.contrib import admin
+# from django.urls import path, include
+#
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path(r'^user/',include('apps.users.urls',namespace='user')),   # 用户模块  namespace = //反向解析
+#     path(r'^cart/',include('apps.cart.urls',namespace='cart')),   # 购物车模块
+#     path(r'^order/',include('apps.order.urls',namespace='order')),   # 订单模块
+#     path(r'^',include('apps.goods.urls',namespace='goods')),   # 商品模块
+# ]
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # 富文本编辑器注册
+    re_path('tinymce/', include('tinymce.urls')),
+    # 全文检索框架
+    # re_path('search/', include('haystack.urls')),
+
+    # 注册自定义应用的路由
+    re_path('user/', include(('apps.users.urls', 'user'), namespace='user')),
+    re_path('cart/', include(('apps.cart.urls', 'cart'), namespace='cart')),
+    re_path('order/', include(('apps.order.urls', 'order'), namespace='order')),
+    re_path(r'^', include(('apps.goods.urls', 'goods'), namespace='goods')),
 ]
